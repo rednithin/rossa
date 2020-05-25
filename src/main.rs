5159002f8058057b31642a56f0f9fdf1badd4d15
rednithin <reddy.nithinpg@live.com>
2020-05-25 22:52:43 +0530

    Taking care of values which contain space in them.

--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,35 @@ fn with_tera(
     warp::any().map(move || tera.clone())
 }
 
+fn append_frag(text: &mut String, frag: &mut String) {
+    if !frag.is_empty() {
+        let encoded = frag
+            .chars()
+            .collect::<Vec<char>>()
+            .chunks(2)
+            .map(|ch| u8::from_str_radix(&ch.iter().collect::<String>(), 16).unwrap())
+            .collect::<Vec<u8>>();
+        text.push_str(&std::str::from_utf8(&encoded).unwrap());
+        frag.clear();
+    }
+}
+
+fn url_decode(text: &str) -> String {
+    let mut output = String::new();
+    let mut encoded_ch = String::new();
+    let mut iter = text.chars();
+    while let Some(ch) = iter.next() {
+        if ch == '%' {
+            encoded_ch.push_str(&format!("{}{}", iter.next().unwrap(), iter.next().unwrap()));
+        } else {
+            append_frag(&mut output, &mut encoded_ch);
+            output.push(ch);
+        }
+    }
+    append_frag(&mut output, &mut encoded_ch);
+    output
+}
+
 #[tokio::main(core_threads = 1)]
 async fn main() {
     dotenv::dotenv().unwrap_or_default();
@@ -91,6 +120,7 @@ async fn main() {
         .and(warp::any().map(move || files_prefix.clone()))
         .and_then(
             |path: FullPath, tera: Arc<Tera>, files_prefix: String| async move {
+                let path = url_decode(path.as_str());
                 if let Ok(mut entries) = fs::read_dir(".".to_string() + path.as_str()).await {
                     log::info!("entries {:?}", entries);
 
